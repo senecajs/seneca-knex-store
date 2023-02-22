d04e6ed65246a8cdd9fd27a6fdf1505807e5a8e5
José Perigolo Neto <neto.perigolo@gmail.com>
2023-02-22 16:48:51 -0300

    Update intern.ts

--- src/intern.ts
+++ src/intern.ts
@@ -1,219 +1,179 @@
+const { Q, transactionQuery} = require('./qbuilder')
+
 export class intern {
-  static is_new(ent: any): boolean {
-    // NOTE: This function is intended for use by the #save method. This
-    // function returns true when the entity argument is assumed to not yet
-    // exist in the store.
-    //
-    // In terms of code, if client code looks like so:
-    // ```
-    //   seneca.make('product')
-    //     .data$({ label, price })
-    //     .save$(done)
-    // ```
-    //
-    // - `is_new` will be invoked from the #save method and return
-    // true, because the product entity is yet to be saved.
-    //
-    // The following client code will cause `is_new` to return false,
-    // when invoked from the #save method, because the user entity already
-    // exists:
-    // ```
-    //   seneca.make('user')
-    //     .load$(user_id, (err, user) => {
-    //       if (err) return done(err)
-    //
-    //       return user
-    //         .data$({ email, username })
-    //         .save$(done)
-    //     })
-    // ```
-    //
-    return null != ent && null == ent.id
-  }
+  static async find_knex(ent: any, q: any): Promise<any> {
+    const ent_table = intern.tablenameUtil(ent)
 
-  static is_upsert(msg: any): boolean {
-    const { ent, q } = msg
-    return intern.is_new(ent) && q && Array.isArray(q.upsert$)
-  }
-
-  static find_mement(entmap: any, base_ent: any, filter: any): any {
-    const { base, name } = base_ent.canon$({ object: true })
-    const entset = entmap[base] && entmap[base][name]
-
-    if (null == entset) {
-      return null
+    const args = {
+      table_name: ent_table,
+      id: q
     }
 
-    let out = null
+    const query = Q.select(args)
+    return query
 
-    for (const ent_id in entset) {
-      const mement = entset[ent_id]
+  }
 
-      if (matches(mement, filter)) {
-        out = mement
-        break
-      }
+  static async insert_knex(ent: any, data: any): Promise<any> {
+    const ent_table = intern.tablenameUtil(ent)
+
+    const args = {
+      table_name: ent_table,
+      data: data
     }
 
-    return out
+    const query = Q.insert(args)
+    return query
+  }
 
-    function matches(ent: any, filter: any): boolean {
-      for (const fp in filter) {
-        if (fp in ent && filter[fp] === ent[fp]) {
-          continue
-        }
+  static async update_knex(ent: any, data: any, q: any): Promise<any> {
+    const ent_table = intern.tablenameUtil(ent)
 
-        return false
-      }
+    const args = {
+      table_name: ent_table,
+      data: data,
+      id: q
+    }
 
+    const query = Q.update(args)
+    return query
+  }
+
+  static async remove_knex(ent: any, q: any): Promise<any> {
+    const ent_table = intern.tablenameUtil(ent)
+
+    const args = {
+      table_name: ent_table,
+      id: q
+    }
+
+    const query = Q.delete(args)
+    return query
+  }
+
+  static async upsert_knex(ent: any, data: any, q: any): Promise<any> {
+    const ent_table = intern.tablenameUtil(ent)
+
+    const args = {
+      table_name: ent_table,
+      data: data,
+      id: q
+    }
+
+    const query = Q.upsert(args)
+    return query
+  }
+
+  static async find_knex_transaction(ent: any, q: any): Promise<any> {
+    const ent_table = intern.tablenameUtil(ent)
+
+    const args = {
+      table_name: ent_table,
+      id: q
+    }
+
+    const query = transactionQuery.select(args)
+    return query
+
+  }
+
+  static async insert_knex_transaction(ent: any, data: any): Promise<any> {
+    const ent_table = intern.tablenameUtil(ent)
+
+    const args = {
+      table_name: ent_table,
+      data: data
+    }
+
+    const query = transactionQuery.insert(args)
+    return query
+  }
+
+  static async update_knex_transaction(ent: any, data: any, q: any): Promise<any> {
+    const ent_table = intern.tablenameUtil(ent)
+
+    const args = {
+      table_name: ent_table,
+      data: data,
+      id: q
+    }
+
+    const query = transactionQuery.update(args)
+    return query
+  }
+
+  static async remove_knex_transaction(ent: any, q: any): Promise<any> {
+    const ent_table = intern.tablenameUtil(ent)
+
+    const args = {
+      table_name: ent_table,
+      id: q
+    }
+
+    const query = transactionQuery.delete(args)
+    return query
+  }
+
+  static async upsert_knex_transaction(ent: any, data: any, q: any): Promise<any> {
+    const ent_table = intern.tablenameUtil(ent)
+
+    const args = {
+      table_name: ent_table,
+      data: data,
+      id: q
+    }
+
+    const query = transactionQuery.upsert(args)
+    return query
+  }
+
+  static tablenameUtil(ent: any) {
+    const canon = ent.canon$({ object: true })
+
+    return (canon.base ? canon.base + '_' : '') + canon.name
+  }
+
+  static async is_new(ent: any) {
+    const isNew = await intern.find_knex(ent, ent.id)
+
+    if (isNew) {
       return true
     }
+
+    return false
   }
 
-  static update_mement(
-    entmap: any,
-    base_ent: any,
-    filter: any,
-    new_attrs: any
-  ) {
-    const ent_to_update = intern.find_mement(entmap, base_ent, filter)
+  static getConfig(spec: any) {
+    let conf
 
-    if (ent_to_update) {
-      Object.assign(ent_to_update, new_attrs)
-      return ent_to_update
-    }
+    if ('string' === typeof spec) {
+      const urlM = /^postgres:\/\/((.*?):(.*?)@)?(.*?)(:?(\d+))?\/(.*?)$/.exec(spec)
 
-    return null
-  }
-
-  static should_merge(ent: any, plugin_opts: any): boolean {
-    return !(false === plugin_opts.merge || false === ent.merge$)
-  }
-
-  // NOTE: Seneca supports a reasonable set of features
-  // in terms of listing. This function can handle
-  // sorting, skiping, limiting and general retrieval.
-  //
-  static listents(seneca: any, entmap: any, qent: any, q: any, done: any) {
-    let list = []
-
-    let canon = qent.canon$({ object: true })
-    let base = canon.base
-    let name = canon.name
-
-    let entset = entmap[base] ? entmap[base][name] : null
-    let ent
-
-    if (null != entset && null != q) {
-      if ('string' == typeof q) {
-        ent = entset[q]
-        if (ent) {
-          list.push(ent)
-        }
-      } else if (Array.isArray(q)) {
-        q.forEach(function (id) {
-          let ent = entset[id]
-          if (ent) {
-            ent = qent.make$(ent)
-            list.push(ent)
-          }
-        })
-      } else if ('object' === typeof q) {
-        let entids = Object.keys(entset)
-        next_ent: for (let id of entids) {
-          ent = entset[id]
-          for (let p in q) {
-            let qv = q[p] // query val
-            let ev = ent[p] // ent val
-
-            if (-1 === p.indexOf('$')) {
-              if (Array.isArray(qv)) {
-                if (-1 === qv.indexOf(ev)) {
-                  continue next_ent
-                }
-              } else if (intern.is_object(qv)) {
-                // mongo style constraints
-                if (
-                  (null != qv.$ne && qv.$ne == ev) ||
-                  (null != qv.$gte && qv.$gte > ev) ||
-                  (null != qv.$gt && qv.$gt >= ev) ||
-                  (null != qv.$lt && qv.$lt <= ev) ||
-                  (null != qv.$lte && qv.$lte < ev) ||
-                  (null != qv.$in && -1 === qv.$in.indexOf(ev)) ||
-                  (null != qv.$nin && -1 !== qv.$nin.indexOf(ev)) ||
-                  false
-                ) {
-                  continue next_ent
-                }
-              } else {
-                if (intern.is_date(qv)) {
-                  if (!(intern.is_date(ev) && intern.eq_dates(qv, ev))) {
-                    continue next_ent
-                  }
-                } else if (qv !== ev) {
-                  continue next_ent
-                }
-              }
-            }
-          }
-          ent = qent.make$(ent)
-          list.push(ent)
-        }
+      if (!urlM)
+      {
+        return null
       }
-    }
-
-    // Always sort first, this is the 'expected' behaviour.
-    if (null != q && q.sort$) {
-      let sf: any
-      for (sf in q.sort$) {
-        break
+      conf = {
+        name: urlM[7],
+        host: urlM[4],
+        username: urlM[2],
+        password: urlM[3],
+        port: urlM[6] ? parseInt(urlM[6], 10) : null,
       }
 
-      let sd = q.sort$[sf] < 0 ? -1 : 1
-      list = list.sort(function (a, b) {
-        return sd * (a[sf] < b[sf] ? -1 : a[sf] === b[sf] ? 0 : 1)
-      })
+    } else {
+      conf = spec
     }
 
-    // Skip before limiting.
-    if (null != q && q.skip$ && q.skip$ > 0) {
-      list = list.slice(q.skip$)
-    }
+    // pg conf properties
+    conf.user = conf.username
+    conf.database = conf.name
 
-    // Limited the possibly sorted and skipped list.
-    if (null != q && q.limit$ && q.limit$ >= 0) {
-      list = list.slice(0, q.limit$)
-    }
+    conf.host = conf.host || conf.server
+    conf.username = conf.username || conf.user
+    conf.password = conf.password || conf.pass
 
-    // Prune fields
-    if (null != q && q.fields$) {
-      for (let i = 0; i < list.length; i++) {
-        let entfields = list[i].fields$()
-        for (let j = 0; j < entfields.length; j++) {
-          if ('id' !== entfields[j] && -1 == q.fields$.indexOf(entfields[j])) {
-            delete list[i][entfields[j]]
-          }
-        }
-      }
-    }
-
-    // Return the resulting list to the caller.
-    done.call(seneca, null, list)
+    return conf
   }
 
-  static clean_array(ary: string[]): string[] {
-    return ary.filter((prop: string) => !prop.includes('$'))
-  }
-
-  static is_object(x: any): boolean {
-    return '[object Object]' === toString.call(x)
-  }
-
-  static is_date(x: any): boolean {
-    return '[object Date]' === toString.call(x)
-  }
-
-  static eq_dates(lv: Date, rv: Date): boolean {
-    return lv.getTime() === rv.getTime()
-  }
 }
