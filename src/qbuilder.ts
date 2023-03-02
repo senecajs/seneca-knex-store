0e27f44ed6b863591894e96ff543afb4131dac57
José Perigolo Neto <neto.perigolo@gmail.com>
2023-03-02 07:26:41 -0300

    Update qbuilder.ts
    Remove knex dependency

--- src/qbuilder.ts
+++ src/qbuilder.ts
@@ -1,3 +1,15 @@
+
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        port : 5432,
+        user : 'senecatest',
+        password : 'senecatest_0102',
+        database : 'senecatest_knex'
+    }
+});
+
 interface knex_query {
     upsert(args: { table_name: string, data: any, id: string }): void
     update(args: { table_name: string, data: any, id: string }): void
@@ -7,34 +19,34 @@ interface knex_query {
 }
 
 const Q = {
-    upsert(args: { table_name: string, data: any, id: string }, knex: any){          
+    upsert(args: { table_name: string, data: any, id: string }){          
         knex(args.table_name)
         .upsert(args.data, args.id)
     },
-    update(args: { table_name: string, data: any, id: string }, knex: any){
+    update(args: { table_name: string, data: any, id: string }){
         knex(args.table_name)
         .update(args.data)
         .where(args.id)
     },
-    insert(args: { table_name: string, data: any }, knex: any){
+    insert(args: { table_name: string, data: any }){
         knex(args.table_name)
         .insert(args.data)
     },
-    delete(args: { table_name: string, id: string }, knex: any){
+    delete(args: { table_name: string, id: string }){
         knex(args.table_name)
         .delete()
         .where(args.id)
     },
-    select(args: { table_name: string, id: string }, knex: any){
+    select(args: { table_name: string, id: string }){
         knex(args.table_name)
         .select()
         .where(args.id)
     },
-    first(args: { table_name: string, id: string }, knex: any){
+    first(args: { table_name: string, id: string }){
         knex(args.table_name)
         .first()
         .where(args.id)
     },
 }
 
-module.exports = Q
+export default Q
