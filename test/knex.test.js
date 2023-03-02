723e897ee78b86b0dfe8ab14844d898b241d83df
José Perigolo Neto <neto.perigolo@gmail.com>
2023-03-02 07:25:59 -0300

    Updating tests

--- test/knex.test.js
+++ test/knex.test.js
@@ -1,44 +1,75 @@
-/*
-  MIT License,
-  Copyright (c) 2010-2022, Richard Rodger and other contributors.
-*/
-
-'use strict'
-
-const Seneca = require('seneca')
-const Shared = require('seneca-store-test')
-
+const seneca = require('seneca')
+// const Shared = require('seneca-store-test')
 const Lab = require('@hapi/lab')
 const lab = (exports.lab = Lab.script())
+const { describe, it, before } = lab
+const { expect } = require('code')
 
-//TO DO - KNEX TESTS
+const KnexStore = require('../src/knex-store')
+const DbConfig = require('./config/database/config')
 
-const seneca = Seneca({
-  log: 'silent',
-  default_plugins: { 'knex-store': false },
+describe('knex-store tests', function () {
+  const test = seneca()
+    .test()
+    .use('entity')
+    .use(KnexStore, { DbConfig })
+    .use('promisify')
+
+  console.log(test)
+
+  before(() => {
+    return new Promise((done) => {
+      seneca().ready(done)
+    })
+  })
+
+  it('save', async () => {
+    const s0 = await seneca().entity().begin()
+    console.log(s0)
+    const foo1 = await s0.entity('foo').data$({ x: 1 }).save$()
+    console.log(foo1)
+    const tx0 = await s0.entity.end()
+
+    expect(tx0).include({
+      begin: { handle: { name: 'postgres' } },
+      canon: {},
+      handle: { name: 'postgres' },
+      trace: [ { msg: {}, meta: {} } ],
+      end: { done: true },
+    })
+
+
+  })
+
+  it('load', async () => {
+    const s0 = await seneca().entity.begin()
+    console.log(s0)
+    let rows = seneca.entity('foo').list$()
+    console.log(rows) /// lists rows
+
+    expect(rows.length).equal(1)
+    expect(rows[0].x).equal('1')
+  })
+
+  it('update', async () => {
+    const s0 = await seneca().entity.begin()
+    console.log(s0)
+    const foo1 = await s0.entity('foo').data$({ x: 5 }).save$()
+    console.log(foo1)
+
+    expect('foo').to.exist()
+    expect(typeof 'foo'.id).to.equal('number')
+    expect('foo'.x).to.equal(5)
+  })
+
+  it('remove', async () => {
+    const s0 = await seneca().entity.begin()
+    console.log(s0)
+    const foo1 = await s0.entity('foo').data$({ x: 5 }).remove$()
+    console.log(foo1)
+
+    expect('foo').to.exist()
+    expect(typeof 'foo'.id).to.equal('number')
+    expect('foo'.x).to.be.false
+  })
 })
-seneca.use({ name: '..', tag: '1' })
-
-const senecaMerge = Seneca({
-  log: 'silent',
-})
-senecaMerge.use({ name: '..', tag: '1' }, { merge: false })
-
-if ('2.0.0' <= seneca.version) {
-  seneca.use('entity', { knex: false })
-  senecaMerge.use('entity', { knex_store: false })
-}
-
-const seneca_test = Seneca() // ({ require })
-  .test()
-  .use('promisify')
-  .use('entity', { knex_store: false })
-//.use('..')
-
-const test_opts = {
-  seneca: seneca_test,
-  name: 'knex-store',
-}
-
-Shared.test.init(lab, test_opts)
-Shared.test.keyvalue(lab, test_opts)
