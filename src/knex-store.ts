06c7ce70ff38aef75e7654b170dddd4f3d982ded
José Perigolo Neto <neto.perigolo@gmail.com>
2023-03-02 07:27:49 -0300

    Update knex-store.ts
    Added PG configuration to work with knex

--- src/knex-store.ts
+++ src/knex-store.ts
@@ -1,9 +1,6 @@
 /* Copyright (c) 2010-2022 Richard Rodger and other contributors, MIT License */
 import { intern } from './intern'
-const knex = require('knex')({
-  client: 'pg',
-  version: '8.8.0'
-})
+const Pg = require('pg')
 
 const STORE_NAME = 'knex-store'
 
@@ -20,8 +17,24 @@ function knex_store(this: any, options: Options) {
   // Take a reference to the calling Seneca instance
   const seneca: any = this
   
-  const conf = intern.getConfig(options)
-  knex.conection = conf
+  let dbPool: {
+    end: any ,
+    connect: () => Promise<any> 
+}
+
+  function configure(spec: any, done: any) {
+    const conf = intern.getConfig(spec)
+
+    dbPool = new Pg.Pool({
+      user: conf.user,
+      host: conf.host,
+      database: conf.database,
+      password: conf.password,
+      port: conf.port
+    })
+
+    return done()
+  }
 
   // Define the store using a description object.
   // This is a convenience provided by seneca.store.init function.
@@ -35,6 +48,8 @@ function knex_store(this: any, options: Options) {
       // and the entity to save
       const seneca = this
       const { ent, q } = msg
+      console.log(ent)
+      console.log(meta)
       
       const ctx = intern.buildCtx(seneca, msg, meta)
 
@@ -48,26 +63,26 @@ function knex_store(this: any, options: Options) {
       return is_new ? do_create() : do_save()
 
       // Create a new entity
-      function do_create() {
+      async function do_create() {
         // generate a new id for the entity
         const id = options.generate_id(ent)
 
         // create a new entity
-        return intern
-          .insertKnex(ent, id)
-          .then((result: any) => {
-            // update the entity with the new id
-            ent.id = id
+        try {
+          const result = await intern
+            .insertKnex(ent, id)
+            console.log(result)
+          // update the entity with the new id
+          ent.id = id
 
-            // call the reply callback with the
-            // updated entity
-            reply(null, ent)
-          })
-          .catch((err: Error) => {
-            // if there is an error, call the reply
-            // callback with the error
-            reply(err)
-          })
+          // call the reply callback with the
+          // updated entity
+          reply(null, ent)
+        } catch (err) {
+          // if there is an error, call the reply
+          // callback with the error
+          reply(err)
+        }
       }
 
       // Save an existing entity  
@@ -108,34 +123,23 @@ function knex_store(this: any, options: Options) {
         q)
       reply(null, list)
     },
+
+    native: function (_msg: any, done: any) {
+      dbPool.connect().then(done).catch(done)
+    },
+
+    close: function (_msg: any, done: any) {
+      dbPool.end().then(done).catch(done)
+    },
   }
-
-
-  seneca.add(
-    { role: store.name, cmd: 'dump' },
-    function (_msg: any, reply: any) {
-      reply()
-    }
-  )
-
-  seneca.add(
-    { role: store.name, cmd: 'export' },
-    function (_msg: any, reply: any) {
-      reply()
-    }
-  )
-
-  // TODO: support direct import of literal objects
-  seneca.add(
-    { role: store.name, cmd: 'import' },
-    function (this: any, msg: any, reply: any) {
-      reply()
-    }
-  )
 
   // Seneca will call init:plugin-name for us. This makes
   // this action a great place to do any setup.
   const meta = seneca.store.init(seneca, options, store)
+
+  seneca.add({ init: store.name, tag: meta.tag }, function (_msg: any, done: any) {
+    return configure(options, done)
+  })
 
   // We don't return the store itself, it will self load into Seneca via the
   // init() function. Instead we return a simple object with the stores name
@@ -146,25 +150,4 @@ function knex_store(this: any, options: Options) {
   }
 }
 
-module.exports.preload = function () {
-  let seneca = this
-
-  let meta = {
-    name: STORE_NAME,
-    exportmap: {
-      native: function () {
-        seneca.export(STORE_NAME + '/native').apply(this, arguments)
-      },
-    },
-  }
-
-  return meta
-}
-
 module.exports = knex_store
-Object.defineProperty(module.exports, 'name', { value: 'knex-store' })
-
-module.exports.defaults = {
-  'entity-id-exists':
-    'Entity of type <%=type%> with id = <%=id%> already exists.',
-}
