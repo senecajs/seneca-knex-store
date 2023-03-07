ea37d4142637724fd95a568401db09704ebd0c82
José Perigolo Neto <neto.perigolo@gmail.com>
2023-03-07 14:31:33 -0300

    Update qbuilder.ts
    Updated to use PG
    Removed unecessary promises

--- src/qbuilder.ts
+++ src/qbuilder.ts
@@ -1,69 +1,33 @@
-
 const knex = require('knex')({
-    client: 'pg',
-    connection: {
-        host : '127.0.0.1',
-        port : 5433,
-        user : 'senecatest',
-        password : 'senecatest_0102',
-        database : 'senecatest_knex'
-    },
-    debug: true
-});
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: 5433,
+    user: 'senecatest',
+    password: 'senecatest_0102',
+    database: 'senecatest_knex',
+  }
+})
 
 const Q = {
-    upsert(args: { table_name: string, data: any, id: string }){          
-        knex(args.table_name)
-        .upsert(args.data, args.id)
-        .then(
-            knex.select('*').from(args.table_name)
-            .then((res: any) => res)
-        )
-    },
-    update(args: { table_name: string, data: any }){
-        knex(args.table_name)
-        .update(args.data)
-        .where(args.data.id)
-        .then(
-            knex.select('*').from(args.table_name)
-            .then((res: any) => res)
-        )
-    },
-    insert(args: { table_name: string, data: any}){
-        knex(args.table_name)
-        .insert(args.data)
-        .then(
-            knex.select('*').from(args.table_name)
-            .then((res: any) => res)
-        )
-    },
-    delete(args: { table_name: string, id: string }){
-        knex(args.table_name)
-        .delete()
-        .where(args.id)
-        .then(
-            knex.select('*').from(args.table_name)
-            .then((res: any) => res)
-        )
-    },
-    select(args: { table_name: string, id: string }){
-        knex(args.table_name)
-        .select()
-        .where(args.id)        
-        .then(
-            knex.select('*').from(args.table_name)
-            .then((res: any) => res)
-        )
-    },
-    first(args: { table_name: string, id: string }){
-        knex(args.table_name)
-        .first()
-        .where(args.id)
-        .then(
-            knex.select('*').from(args.table_name)
-            .then((res: any) => res)
-        )
-    },
+  upsert(args: { table_name: string; data: any; id: string }) {
+    return knex(args.table_name).upsert(args.data, args.id)
+  },
+  update(args: { table_name: string; data: any }) {
+    return knex(args.table_name).update(args.data).where(args.data.id)
+  },
+  insert(args: { table_name: string; data: any }) {
+    return knex(args.table_name).insert(args.data)
+  },
+  delete(args: { table_name: string; id: string }) {
+    return knex(args.table_name).delete().where(args.id)
+  },
+  select(args: { table_name: string}) {
+    return knex().select().from(args.table_name)
+  },
+  first(args: { table_name: string; id: string }) {
+    return knex(args.table_name).first().where(args.id)
+  },
 }
 
 export default Q
