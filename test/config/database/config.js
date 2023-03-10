4d6567c472001a8033668149f1e0bbbddfec7b1b
José Perigolo Neto <neto.perigolo@gmail.com>
2023-03-10 16:06:54 -0300

    Update test corrections

--- test/config/database/config.js
+++ test/config/database/config.js
@@ -1,38 +1,38 @@
 function getConfig() {
   if (process.env.SENECA_KNEX_TEST_CONFIG) {
     return {
-      // name: "senecatest_knex_ci",
-      // host: "localhost",
-      // port: 5433,
-      // username: "senecatest",
-      // password: "senecatest_ci_0102",
-      // options: {}
-      client: 'pg',
-      connection: {
-        host: '127.0.0.1',
-        port: 5433,
-        user: 'senecatest',
-        password: 'senecatest_0102',
-        database: 'senecatest_knex',
-      },
+      name: "senecatest_knex_ci",
+      host: "localhost",
+      port: 5433,
+      username: "senecatest",
+      password: "senecatest_ci_0102",
+      options: {}
+      // client: 'pg',
+      // connection: {
+      //   host: '127.0.0.1',
+      //   port: 5433,
+      //   user: 'senecatest',
+      //   password: 'senecatest_0102',
+      //   database: 'senecatest_knex',
+      // },
     }
   }
 
   return {
-    // name: "senecatest_knex",
-    // host: "localhost",
-    // port: 5433,
-    // username: "senecatest",
-    // password: "senecatest_0102",
-    // options: {},
-    client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      port: 5433,
-      user: 'senecatest',
-      password: 'senecatest_0102',
-      database: 'senecatest_knex',
-    },
+    name: "senecatest_knex",
+    host: "localhost",
+    port: 5433,
+    username: "senecatest",
+    password: "senecatest_0102",
+    options: {},
+    // client: 'pg',
+    // connection: {
+    //   host: '127.0.0.1',
+    //   port: 5433,
+    //   user: 'senecatest',
+    //   password: 'senecatest_0102',
+    //   database: 'senecatest_knex',
+    // },
   }
 }
 
