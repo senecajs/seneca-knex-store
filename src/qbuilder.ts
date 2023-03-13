a7a7ef96dfae2e1409803174802842805e352eb0
José Perigolo Neto <neto.perigolo@gmail.com>
2023-03-13 11:34:49 -0300

    Update qbuilder.ts
    new fixes files - all basic tests passing

--- src/qbuilder.ts
+++ src/qbuilder.ts
@@ -23,13 +23,19 @@ const Q = {
   insert(args: { table_name: string; data: any }) {
     return knex(args.table_name).insert(args.data).returning('*')
   },
-  delete(args: { table_name: string; id: string }) {
-    return knex(args.table_name).where({ id: args.id }).del()
+  delete(args: { table_name: string; filter: string, isLoadDeleted: boolean }) {
+    if (args.isLoadDeleted){
+      return knex(args.table_name).where(args.filter).del().returning('*')
+    }
+    return knex(args.table_name).where(args.filter).del()
   },
   truncate(args: { table_name: string }) {
     return knex(args.table_name).truncate()
   },
-  select(args: { table_name: string; data: any }) {
+  select(args: { table_name: string; data: any, isArray: boolean }) {
+    if(args.isArray){
+      return knex(args.table_name).whereIn('id', args.data)
+    }
     return args.data
       ? knex(args.table_name).select().where(args.data)
       : knex.select('*').from(args.table_name)
